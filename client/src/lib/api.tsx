e9e6a499da725a4303634f8ad5048545a64bfbc4
miningforuae <miningforuae@gmail.com>
2025-11-21 17:25:43 +0500

    Delete client directory

--- client/src/lib/api.tsx
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
-
-export const api = axios.create({
-  baseURL: `${BASE_URL}/api/v1`,
-});
-
-export default api;
